d6dac7ee566e6d309a4c2d91b502dfbd1ff684d3
Matt Briggs <matt@mattbriggs.net>
2012-08-07 13:21:38 -0400

    many things

--- toggle_last_view.py
+++ toggle_last_view.py
@@ -2,9 +2,15 @@ import sublime_plugin
 
 
 class ToggleLastViewListener(sublime_plugin.EventListener):
+    deactivated = None
+
     def on_deactivated(self, view):
         if view.file_name():
-            ToggleLastViewCommand.previous_file = view.file_name()
+            self.deactivated = view.file_name()
+
+    def on_activated(self, view):
+        if (self.deactivated is not None) and (self.deactivated != view.file_name()):
+            ToggleLastViewCommand.previous_file = self.deactivated
 
 
 class ToggleLastViewCommand(sublime_plugin.WindowCommand):
