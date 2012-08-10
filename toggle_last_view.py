25a8e35a3f19437a740bb749a2832a01df206802
Matt Briggs <matt@mattbriggs.net>
2012-08-10 10:06:19 -0400

    small things

--- toggle_last_view.py
+++ toggle_last_view.py
@@ -5,18 +5,21 @@ class ToggleLastViewListener(sublime_plugin.EventListener):
     deactivated = None
 
     def on_deactivated(self, view):
-        if view.file_name():
-            self.deactivated = view.file_name()
+        print 'deactivated'
+        print view
+        self.deactivated = view
 
     def on_activated(self, view):
-        if (self.deactivated is not None) and (self.deactivated != view.file_name()):
-            ToggleLastViewCommand.previous_file = self.deactivated
+        print 'ACTIVATED'
+        print view
+        if (self.deactivated is not None) and (self.deactivated != view):
+            ToggleLastViewCommand.last_view = self.deactivated
 
 
 class ToggleLastViewCommand(sublime_plugin.WindowCommand):
-    previous_file = None
+    last_view = None
 
     def run(self):
-        prev = self.previous_file
+        prev = self.last_view
         if prev:
-            self.window.open_file(prev)
+            self.window.focus_view(self.last_view)
